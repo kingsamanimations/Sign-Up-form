04dcf24185ca9a3377731f7b48192916466ceefe
King Sam Animations <224757196+kingsamanimations@users.noreply.github.com>
2026-03-12 11:41:22 +0000

    Made adjustments to main.js and index.html

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 // Check if password and confirm password match
 const password = document.getElementById("user_password");
-const confirmpassword = document.getElementById("confirm_password");
+const confirmPassword = document.getElementById("confirm_password");
+const form = document.getElementById("signup-form");
 
-confirmpassword.addEventListener("change", () => {
-    if (password.value !== confirmpassword.value) {
-        confirmpassword.setCustomValidity("Passwords do not match!");
+confirmPassword.addEventListener("input", () => {
+    if (password.value !== confirmPassword.value) {
+        confirmPassword.style.boxShadow = "0 0 8px 2px red";
+        confirmPassword.style.borderColor = "red";
     } else {
-        confirmpassword.setCustomValidity("");
+        confirmPassword.style.boxShadow = "0 0 8px 2px green";
+        confirmPassword.style.borderColor = "green";
     }
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (password.value !== confirmPassword.value) {
+        alert("Passwords do not match!");
+        return; // Stops here - doesn't send anything
+    }
+    alert("Account created successfully!"); // Paswords match - valid form
 });
